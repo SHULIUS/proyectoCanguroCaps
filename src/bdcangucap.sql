040e8bd885692898ef1129e3bf28fae7f4437bcb
SHULIUS <100172561+SHULIUS@users.noreply.github.com>
2023-05-06 00:07:29 -0500

    second commit

---
+++ src/bdcangucap.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE canguroCaps;
+
+USE canguroCaps;
+
+CREATE schema Admin;
+
+CREATE TABLE Admin.inventory;
+CREATE TABLE Admin.sales;
+CREATE TABLE Admin.establishment;
+CREATE TABLE Admin.turns;
+CREATE TABLE Admin.suppliers;
+
+
+
